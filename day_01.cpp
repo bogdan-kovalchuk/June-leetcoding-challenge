b1561b0cd3a45e849e5b7a08324b3d842b51a45f
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-06-01 19:54:42 +0300

    day 1 commit

--- day_01.cpp
+++ day_01.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+
+using std::vector;
 
 //Definition for a binary tree node.
 struct TreeNode {
@@ -16,16 +19,44 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode *invertTree(TreeNode *root) {
+        traverce(root);
         return root;
+    }
+
+    void traverce(TreeNode *root) {
+        if (root == nullptr) return;
+
+        TreeNode *tmp = root->left;
+        root->left = root->right;
+        root->right = tmp;
+
+        traverce(root->right);
+        traverce(root->left);
     }
 };
 
+TreeNode *insertLevelOrder(vector<int> nodes, TreeNode *root, int i, int n) {
+    // Base case for recursion
+    if (i < n && nodes[i] != 0) {
+        auto *temp = new TreeNode(nodes[i]);
+        root = temp;
+
+        // insert left child
+        root->left = insertLevelOrder(nodes, root->left, 2 * i + 1, n);
+
+        // insert right child
+        root->right = insertLevelOrder(nodes, root->right, 2 * i + 2, n);
+    }
+    return root;
+}
+
 int main() {
+    vector<int> nodes = {4, 2, 7, 1, 3, 6, 9};
+
+    TreeNode *root = insertLevelOrder(nodes, root, 0, nodes.size());
+
     Solution solution = Solution();
-    TreeNode *in;
+    TreeNode *out = solution.invertTree(root);
 
-    TreeNode *out = solution.invertTree(in);
-
-//    std::cout << out << std::endl;
     return 0;
 }
