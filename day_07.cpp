98b1dc0e152fcbe14b13dc6aad1f1d78a72f0a56
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-06-07 12:26:27 +0300

    day 7 commit

--- day_07.cpp
+++ day_07.cpp
@@ -8,15 +8,29 @@ using std::map;
 
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        return 0;
+    int change(int amount, vector<int> &coins) {
+        vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, 0));
+
+        for (int i = 0; i <= coins.size(); ++i) dp[i][0] = 1;
+
+        for (int i = 1; i <= coins.size(); ++i) {
+            for (int j = 1; j <= amount; ++j) {
+                if (j < coins[i - 1]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                }
+            }
+        }
+
+        return dp.back().back();
     }
 };
 
 
 int main() {
-    int amount = 5;
-    vector<int> coins = {1, 2, 5};
+    int amount = 0;
+    vector<int> coins = {};
 
     Solution solution = Solution();
     std::cout << solution.change(amount, coins) << std::endl;
