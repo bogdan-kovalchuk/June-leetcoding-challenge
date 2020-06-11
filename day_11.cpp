51fcb9e501b437976a1b0c25a636e4171b2913ce
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-06-11 14:13:13 +0300

    day 11 commit

---
+++ day_11.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+
+
+using std::vector;
+using std::map;
+using std::string;
+
+class Solution {
+public:
+    void sortColors(vector<int> &nums) {
+        int colors[3] = {};
+        for (const auto &n : nums)
+            colors[n]++;
+
+        int k = 0;
+        for (int i = 0; i < sizeof(colors)/sizeof(colors[0]); ++i) {
+            for (int j = 0; j < colors[i]; ++j) {
+                nums[k] = i;
+                k++;
+            }
+        }
+    }
+};
+
+
+int main() {
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+
+    Solution solution = Solution();
+    solution.sortColors(nums);
+
+    for (const auto &n : nums)
+        std::cout << n << " ";
+
+    return 0;
+}
