9f81122d33aaa3b36cac43846a61c21ac0fac46c
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-06-13 15:13:57 +0300

    day 13 commit

--- day_13.cpp
+++ day_13.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-//#include <random>
 #include <set>
 
 
@@ -9,14 +8,38 @@ using std::set;
 
 class Solution {
 public:
-    vector<int> largestDivisibleSubset(vector<int>& nums) {
-        return nums;
+    vector<int> largestDivisibleSubset(vector<int> &nums) {
+        if (nums.size() == 0 || nums.size() == 1) return nums;
+
+        int max_elem = 0;
+        vector<int> dp(nums.size(), 1);
+        vector<int> result;
+
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i < nums.size(); ++i) {
+            for (int j = i - 1; j >= 0; --j) {
+                if (nums[i] % nums[j] == 0) {
+                    if (dp[i] < 1 + dp[j]) dp[i] = 1 + dp[j];
+                }
+            }
+            if (dp[i] > max_elem)
+                max_elem = dp[i];
+        }
+
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (dp[i] == max_elem) {
+                result.push_back(nums[i]);
+                max_elem--;
+            }
+        }
+
+        return result;
     }
 };
 
 
 int main() {
-    vector<int> nums = {1,2,4,8};
+    vector<int> nums = {4, 8, 10, 240};
 
     Solution solution = Solution();
     auto out = solution.largestDivisibleSubset(nums);
