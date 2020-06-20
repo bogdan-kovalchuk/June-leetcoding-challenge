9ae03ebf6e59a56f1e63c4b730e5405f9f25f162
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-06-20 17:39:47 +0300

    day 20 commit

--- day_20.cpp
+++ day_20.cpp
@@ -7,7 +7,27 @@ using std::string;
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        return "";
+        int factorials[n];
+
+        vector<int> list;
+        list.push_back(1);
+
+        factorials[0] = 1;
+        for(int i = 1; i < n; ++i){
+            factorials[i] = factorials[i - 1] * i;
+            list.emplace_back(i + 1);
+        }
+
+        k--;
+        string sb;
+        for (int i = n - 1; i >= 0; i--){
+            int j = k / factorials[i];
+            k -= j * factorials[i];
+
+            sb += std::to_string(list[j]);
+            list.erase(list.begin() + j);
+        }
+        return sb;
     }
 };
 
