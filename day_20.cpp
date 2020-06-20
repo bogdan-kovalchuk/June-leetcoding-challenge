7dd143ac0c7f2f71c2ac6a67e0d0c02377b4bac3
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-06-20 16:56:35 +0300

    day 20 commit

---
+++ day_20.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::string;
+
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        return "";
+    }
+};
+
+int main() {
+    int n = 3, k = 3;
+
+    Solution solution = Solution();
+    std::cout << solution.getPermutation(n, k) << std::endl;
+
+    return 0;
+}
