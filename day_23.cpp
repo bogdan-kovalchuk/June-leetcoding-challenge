97bf8574d450e39d96bf4b7620053327da2e7423
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-06-23 10:21:19 +0300

    day 23 commit

---
+++ day_23.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using std::vector;
+using std::unordered_map;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+
+};
+
+class Solution {
+public:
+    int countNodes(TreeNode *root) {
+
+    }
+};
+
+TreeNode *insertLevelOrder(vector<int> nodes, TreeNode *root, int i, int n) {
+    // Base case for recursion
+    if (i < n && nodes[i] != 0) {
+        auto *temp = new TreeNode(nodes[i]);
+        root = temp;
+
+        // insert left child
+        root->left = insertLevelOrder(nodes, root->left, 2 * i + 1, n);
+
+        // insert right child
+        root->right = insertLevelOrder(nodes, root->right, 2 * i + 2, n);
+    }
+    return root;
+}
+
+
+int main() {
+    vector<int> nodes = {1, 2, 3, 4, 5, 6};
+
+    TreeNode *root = insertLevelOrder(nodes, root, 0, nodes.size());
+
+    Solution solution = Solution();
+    auto out = solution.countNodes(root);
+
+    return 0;
+}
