9c9ee8c6a089e08ce66757338e7a258f566e8db1
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-06-27 11:23:58 +0300

    day 27 commit

---
+++ day_27.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using std::vector;
+using std::unordered_map;
+
+class Solution {
+public:
+class Solution {
+public:
+    int numSquares(int n) {
+        
+    }
+};
