8a53fa6f5f26065bbde2378591d59f27e49ff5ea
bogdan-kovalchuk <bogdan.kovalchuk@globallogic.com>
2020-06-28 18:13:22 +0300

    day 27 commit

--- day_27.cpp
+++ day_27.cpp
@@ -7,9 +7,17 @@ using std::unordered_map;
 
 class Solution {
 public:
-class Solution {
-public:
     int numSquares(int n) {
-        
+        static vector<int> dp{0};
+
+        while (dp.size() <= n) {
+            int j = dp.size();
+            int s = INT_MAX;
+            for (int i = 1; i * i <= j; ++i)
+                s = min(s, dp[j - i * i] + 1);
+            dp.push_back(s);
+        }
+
+        return dp[n];
     }
 };
